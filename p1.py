90ba701d39d93ec0694f36ed625c9f1f296ca08f
ps26 <pravallika-sunkara@users.noreply.github.com>
2016-02-25 22:03:24 -0500

    Create p1.py

---
+++ p1.py
@@ -0,0 +1,21 @@
+'''Problem: Write a program for sorting elements of an array in descending order'''
+
+import random
+def desc(A):
+  temp=0
+  for i in range(0,len(A)):
+    for j in range(0,len(A)):
+      if A[i]>A[j]:
+        temp=A[i]
+        A[i]=A[j]
+        A[j]=temp
+  print "Descending order of elements:", A
+
+if __name__=='__main__':
+  N=int(raw_input("Enter the number of elements in the array:"))
+  A=[]
+  for i in range(0,N):
+    p=random.randint(0,100)
+    A.append(p)
+  print "Original elements in the array:",A
+  desc(A)
