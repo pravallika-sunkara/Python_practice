9a0d4ed649a1bdc4a8d22b521eb26f7be683aba5
ps26 <pravallika-sunkara@users.noreply.github.com>
2018-08-04 13:56:26 -0700

    Create twoSum_leetcode.py

---
+++ twoSum_leetcode.py
@@ -0,0 +1,27 @@
+'''
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Example:
+
+Given nums = [2, 7, 11, 15], target = 9,
+
+Because nums[0] + nums[1] = 2 + 7 = 9,
+return [0, 1].
+
+'''
+
+
+class Solution(object):
+    def twoSum(self,nums,target):
+        for i in nums:
+            j = target - i
+            tmp_nums_start_index = nums.index(i) + 1
+            tmp_nums = nums[tmp_nums_start_index:]
+            if j in tmp_nums:
+                return [nums.index(i), tmp_nums_start_index + tmp_nums.index(j)]
+
+
+if __name__ == "__main__":
+    print(Solution().twoSum([3,4,5,6,7],10))
